a8a5bc294ed1a676c8a21b689d0d1cbbc14236bb
Pablo Martinez <pablet@gmail.com>
2018-03-08 07:14:19 +0100

    changes in form

--- src/form.py
+++ src/form.py
@@ -17,9 +17,9 @@ import wx.xrc
 class MainFrame ( wx.Frame ):
 	
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 647,547 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 661,551 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 		
-		self.SetSizeHints( -1,-1)
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 		
 		bSizer1 = wx.BoxSizer( wx.VERTICAL )
 		
@@ -30,25 +30,70 @@ class MainFrame ( wx.Frame ):
 		bSizer6 = wx.BoxSizer( wx.VERTICAL )
 		
 		bSizer6.SetMinSize( wx.Size( 300,-1 ) ) 
-		self.devInfo_button = wx.Button( self, wx.ID_ANY, u"Device Info", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer6.Add( self.devInfo_button, 0, wx.ALL, 5 )
+		self.m_notebook1 = wx.Notebook( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0, u"solapas" )
+		self.m_panel1 = wx.Panel( self.m_notebook1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL, u"Commands" )
+		bSizer4 = wx.BoxSizer( wx.VERTICAL )
 		
-		self.m_button7 = wx.Button( self, wx.ID_ANY, u"MyButton", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer6.Add( self.m_button7, 0, wx.ALL, 5 )
+		self.m_button5 = wx.Button( self.m_panel1, wx.ID_ANY, u"Device Info", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer4.Add( self.m_button5, 0, wx.ALL, 5 )
+		
+		self.m_button6 = wx.Button( self.m_panel1, wx.ID_ANY, u"Status", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer4.Add( self.m_button6, 0, wx.ALL, 5 )
+		
+		self.m_staticText3 = wx.StaticText( self.m_panel1, wx.ID_ANY, u"Run script", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText3.Wrap( -1 )
+		bSizer4.Add( self.m_staticText3, 0, wx.ALL, 5 )
+		
+		m_comboBox3Choices = []
+		self.m_comboBox3 = wx.ComboBox( self.m_panel1, wx.ID_ANY, u"Select script...", wx.DefaultPosition, wx.DefaultSize, m_comboBox3Choices, 0 )
+		self.m_comboBox3.SetMinSize( wx.Size( 160,-1 ) )
+		
+		bSizer4.Add( self.m_comboBox3, 0, wx.ALL, 5 )
+		
+		self.run_script_button = wx.Button( self.m_panel1, wx.ID_ANY, u"Run", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer4.Add( self.run_script_button, 0, wx.ALL, 5 )
 		
 		
-		fgSizer3.Add( bSizer6, 1, wx.EXPAND, 5 )
+		self.m_panel1.SetSizer( bSizer4 )
+		self.m_panel1.Layout()
+		bSizer4.Fit( self.m_panel1 )
+		self.m_notebook1.AddPage( self.m_panel1, u"a page", False )
+		self.m_panel2 = wx.Panel( self.m_notebook1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer5 = wx.BoxSizer( wx.VERTICAL )
+		
+		self.m_staticText4 = wx.StaticText( self.m_panel2, wx.ID_ANY, u"Direct commands", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText4.Wrap( -1 )
+		bSizer5.Add( self.m_staticText4, 0, wx.ALL, 5 )
+		
+		self.m_textCtrl4 = wx.TextCtrl( self.m_panel2, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_textCtrl4.SetMinSize( wx.Size( 160,-1 ) )
+		
+		bSizer5.Add( self.m_textCtrl4, 0, wx.ALL, 5 )
+		
+		self.send_cmd_button = wx.Button( self.m_panel2, wx.ID_ANY, u"Send", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer5.Add( self.send_cmd_button, 0, wx.ALL, 5 )
+		
+		
+		self.m_panel2.SetSizer( bSizer5 )
+		self.m_panel2.Layout()
+		bSizer5.Fit( self.m_panel2 )
+		self.m_notebook1.AddPage( self.m_panel2, u"a page", True )
+		
+		bSizer6.Add( self.m_notebook1, 1, wx.EXPAND |wx.ALL, 5 )
+		
+		
+		fgSizer3.Add( bSizer6, 1, wx.EXPAND|wx.TOP|wx.BOTTOM|wx.LEFT, 5 )
 		
 		bSizer7 = wx.BoxSizer( wx.VERTICAL )
 		
-		self.log_text = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 300,400 ), 0 )
-		bSizer7.Add( self.log_text, 0, wx.ALL, 5 )
+		self.log_text = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 300,400 ), wx.TE_MULTILINE|wx.TE_READONLY )
+		bSizer7.Add( self.log_text, 1, wx.EXPAND|wx.BOTTOM|wx.RIGHT, 5 )
 		
 		self.clear_button = wx.Button( self, wx.ID_ANY, u"Clear", wx.DefaultPosition, wx.DefaultSize, 0 )
 		bSizer7.Add( self.clear_button, 0, wx.ALL, 5 )
 		
 		
-		fgSizer3.Add( bSizer7, 1, wx.EXPAND, 5 )
+		fgSizer3.Add( bSizer7, 1, wx.EXPAND|wx.TOP|wx.BOTTOM|wx.RIGHT, 5 )
 		
 		
 		bSizer1.Add( fgSizer3, 1, wx.ALIGN_TOP|wx.EXPAND|wx.TOP|wx.RIGHT|wx.LEFT, 5 )
@@ -74,6 +119,8 @@ class MainFrame ( wx.Frame ):
 		fgSizer4.Add( self.conn_button, 0, wx.ALL, 5 )
 		
 		self.m_textCtrl3 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_textCtrl3.SetMinSize( wx.Size( 120,-1 ) )
+		
 		fgSizer4.Add( self.m_textCtrl3, 0, wx.ALL, 5 )
 		
 		
