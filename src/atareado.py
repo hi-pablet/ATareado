ffb9f6cb0aeec0bfc234113b36ab2bd597809061
Pablo Martinez <pabmartinez@fermax.com>
2018-03-13 08:51:43 -0700

    working

--- src/atareado.py
+++ src/atareado.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import wx
+from wx.lib.pubsub import setupkwargs
+from wx.lib.pubsub import pub
 import atframe
 import serialconnection
 from serial.tools.list_ports import comports
@@ -13,29 +15,32 @@ class ATareado(form.MainFrame):
         form.MainFrame.__init__(self, parent)
         self.__serialPort = serialconnection.SerialConnection()
         self.__serialPort.receivedData = self.rxDataCallback
+        self.__serialPort.receivedAnswer = self.rxAnswerCallback
         self.__cmdSent = None
+        pub.subscribe(self.AppendLogText, "AppendLogText")
+        pub.subscribe(self.SetStatusText, "SetStatusText")
 
     def start(self):
         serialPorts = serialconnection.getPortsList()
         self.port_combo.Clear()
         self.port_combo.AppendItems(serialPorts)
-
+        self.setConnectionStatus(False)
 
     def setConnectionStatus(self, status):
         if status:
-            self.m_staticText1.SetLabel("Connected")
+            wx.CallAfter(pub.sendMessage, "SetStatusText", text="Connected")
         else:
-            self.m_staticText1.SetLabel("Disconnected")
+            wx.CallAfter(pub.sendMessage, "SetStatusText", text="Disconnected")
 
     def conn_button_onclick(self, event):
-        self.log_text.AppendText('Conn %s %s '% (self.port_combo.GetValue(), self.m_comboBox2.GetValue()))
+        #self.log_text.AppendText('Conn %s %s '% (self.port_combo.GetValue(), self.m_comboBox2.GetValue()))
         status = self.__serialPort.start(self.port_combo.GetValue(), self.m_comboBox2.GetValue())
         self.setConnectionStatus(status)
 
     def info_button_onclick(self, event):
         if self.__serialPort.status:
             self.log_text.AppendText("Modem Info: \n")
-            cmd = serialconnection.ATcommand('ATI', False, None)
+            cmd = serialconnection.ATcommand(serialconnection.ATI_CMD_ID, True, None)
             self.__serialPort.sendCommand(cmd)
         else:
             self.log_text.AppendText("Serial connection closed\n")
@@ -46,10 +51,26 @@ class ATareado(form.MainFrame):
         else:
             self.log_text.AppendText("Serial connection closed\n")
 
-
     def rxDataCallback(self, data):
         self.log_text.AppendText('\n')
         self.log_text.AppendText(data)
+
+    def rxAnswerCallback(self, answer):
+
+        if answer[0]:
+            text = "Answer:"
+            for t in answer[1]:
+                text += t
+        else:
+            text = "Error:"+answer[1]
+
+        wx.CallAfter(pub.sendMessage, "AppendLogText", text=text)
+
+    def AppendLogText(self, text):
+        self.log_text.AppendText(text)
+
+    def SetStatusText(self, text):
+        self.m_staticText1.SetLabel(text)
 
 
 if __name__ == "__main__":
--- src/serialconnection.py
+++ src/serialconnection.py
@@ -3,7 +3,7 @@ import serial
 import time
 import threading
 import sys
-import pprint
+from pprint import pprint
 import re
 from serial.tools.list_ports import comports
 
@@ -22,57 +22,79 @@ def getPortsList():
 
 AT_CMD_ID = 'AT'
 ATI_CMD_ID = 'ATI'
-                    'CPIN'
-                    'CSQ'
-                    'CGATT'
-                    'CIPMUX'
-                    'CIPMODE'
-                    'CSTT'
-                    'CIICR'
-                    'CIFSR'
-                    'CIPSTART'
-                    'CIPSEND'
-                    'CIPCLOSE'
-                    'CIPCCFG'
+CPIN_CMD_ID = 'CPIN'
+CSQ_CMD_ID = 'CSQ'
+CGATT_CMD_ID = 'CGATT'
+CIPMUX_CMD_ID = 'CIPMUX'
+CIPMODE_CMD_ID = 'CIPMODE'
+CSTT_CMD_ID = 'CSTT'
+CIICR_CMD_ID = 'CIICR'
+CIFSR_CMD_ID = 'CIFSR'
+CIPSTART_CMD_ID = 'CIPSTART'
+CIPSEND_CMD_ID = 'CIPSEND'
+CIPCLOSE_CMD_ID = 'CIPCLOSE'
+CIPCCFG_CMD_ID = 'CIPCCFG'
+
+# Command List [id, command, params, return]
+commandsList = {AT_CMD_ID:     ['AT', '', 'OK'],
+                ATI_CMD_ID:    ['ATI', '', '(.*)OK'],
+                CPIN_CMD_ID:   ['AT+CPIN', '', '+CPIN:(.*)'],
+                CSQ_CMD_ID:    ['AT+CSQ', '', '+CSQ:(\d+)'],
+                CGATT_CMD_ID:  ['AT+CGATT', '', '+CGATT:(\d+)'],
+                CIPMUX_CMD_ID: ['AT+CIPMUX', '', '+CIPMUX'],
+                CIPMODE_CMD_ID:['AT+CIPMODE', '"%val1%,%val2%,%val3%"', 'OK'],
+                CSTT_CMD_ID:   ['AT+CSTT', '"%val1%,%val2%,%val3%"', 'OK'],
+                CIICR_CMD_ID:  ['AT+CIICR', '', 'OK'],
+                CIFSR_CMD_ID:  ['AT+CIFSR', '', '(.*)'],
+                CIPSTART_CMD_ID: ['AT+CIPSTART', '"%val1","%val2","%val3"', 'OK'],  # "[TCP/UDP]", "<ip_dest>", "<port>"
+                CIPSEND_CMD_ID:['AT+CIPSEND', '', '>'],
+                CIPCLOSE_CMD_ID: ['AT+CIPCLOSE', '', 'OK'],
+                CIPCCFG_CMD_ID:['AT+CIPCCFG', '', 'OK']}
+
 
 class ATcommand(object):
-
-    # Command List [id, command, params, return]
-    commandsList = {'AT': ['AT', '', 'OK'],
-                    'ATI': ['ATI', '', '(.*) OK'],
-                    'CPIN': ['AT+CPIN', '', '+CPIN:(.*)'],
-                    'CSQ': ['AT+CSQ', '', '+CSQ:(\d+)'],
-                    'CGATT': ['AT+CGATT', '', '+CGATT:(\d+)'],
-                    'CIPMUX': ['AT+CIPMUX', '', '+CIPMUX'],
-                    'CIPMODE': ['AT+CIPMODE', '"%val1%,%val2%,%val3%"', 'OK'],
-                    'CSTT': ['AT+CSTT', '"%val1%,%val2%,%val3%"', 'OK'],
-                    'CIICR': ['AT+CIICR', '', 'OK'],
-                    'CIFSR': ['AT+CIFSR', '', '(.*)'],
-                    'CIPSTART': ['AT+CIPSTART', '"%val1","%val2","%val3"', 'OK'],  # "[TCP/UDP]", "<ip_dest>", "<port>"
-                    'CIPSEND': ['AT+CIPSEND', '', '>'],
-                    'CIPCLOSE': ['AT+CIPCLOSE', '', 'OK'],
-                    'CIPCCFG': ['AT+CIPCCFG', '', 'OK']}
 
     def __init__(self, cmdId, setMode, params):
         self.cmd = cmdId
         self.setOperation = setMode
         self.params = params
-        self.paramString = ''
-        self.answer = ''
-        self.respError = False
+        try:
+            self.paramString = commandsList[self.cmd][1]
+            self.answer = ''
+            self.respError = False
+            if self.paramString.find('%val1%') != -1 and 0 in params:
+                self.paramString = self.paramString.replace('%val1%', params[0])
+            if self.paramString.find('%val2%') != -1 and 1 in params:
+                self.paramString = self.paramString.replace('%val2%', params[1])
+            if self.paramString.find('%val3%') != -1 and 2 in params:
+                self.paramString = self.paramString.replace('%val3%', params[2])
+        except:
+            print "Exception creating command"
 
     def getSetString(self):
-
-        return commandsList[newCommand.cmd][0] + self.paramString + "\r\n"
+        return commandsList[self.cmd][0] + self.paramString + "\r\n"
 
     def getQueryString(self):
-        return commandsList[newCommand.cmd][0] + "=?\r\n"
+        return commandsList[self.cmd][0] + "=?\r\n"
+
+    def parseResponse(self, response):
+        #print "[%s] search %s " % (response, commandsList[self.cmd][2])
+        searchObj = re.search(commandsList[self.cmd][2], (response.replace('\n', '')).replace('\r', ''), re.S | re.M)
+        if searchObj:
+            pprint(searchObj.groups())
+            #print "Searched ans : ", searchObj.group(1)
+            res = (True, searchObj.groups())
+        else:
+            res = (False, response)
+
+        return res
 
 
 class SerialConnection(object):
 
     #Callback
     receivedData = None
+    receivedAnswer = None
 
     def __init__(self):
         self.status = False
@@ -122,11 +144,13 @@ class SerialConnection(object):
             self.__serial.close()
 
     def _timeoutHandler(self):
-        self.__cmdMutex.acquire()
 
+        print "Command response timeout, command %s" % self.__currCommand.cmd
+        self.__cmdMutex.acquire()
         if self.__waitingAnswer:
             self.__currCommand.respError = True
             self.__waitingAnswer = False
+            self.receivedAnswer((False, "Timeout"))
 
         self.__cmdMutex.release()
 
@@ -142,52 +166,60 @@ class SerialConnection(object):
         self.__cmdMutex.release()
 
         res = True
-        # Known command ?
-        if newCommand.cmd in commandsList:
-            self.__waitingAnswer = True
-            self.__currCommand = newCommand
-            if self.__currCommand.setOperation:
-                self.writeRaw(self.__currCommand.getSetString())
-            self.__TOTimer = threading.Timer(5, self._timeoutHandler)
-            self.__TOTimer.start()
+        self.__waitingAnswer = True
+        self.__currCommand = newCommand
+        if self.__currCommand.setOperation:
+            self.writeRaw(self.__currCommand.getSetString())
         else:
-            res = False
+            self.writeRaw(self.__currCommand.getQueryString())
+        # Start timeout
+        print >> sys.stdout, "Start timeout timer"
+        self.__TOTimer = threading.Timer(5, self._timeoutHandler)
+        self.__TOTimer.start()
 
         return res
 
-    def processAnswer(self, data):
-        res = True
-        searchObj = re.search(commandsList[self.__currCommand.cmd][2], data, re.I)
-        if searchObj:
-            print "Searched ans : ", searchObj.group()
-        return res
 
     def reader(self):
         try:
+            answerString = ''
             while self.status:
                 # read all that is there or wait for one byte
                 print >> sys.stdout, "wait"
-                data = self.__serial.read(self.__serial.in_waiting or 1)
+                readBytes = self.__serial.read(self.__serial.in_waiting or 1)
                 print >> sys.stdout, "rx"
-                if data:
-                    print >> sys.stdout, data
+
+                if readBytes:
+                    print >> sys.stdout, readBytes
                     self.__cmdMutex.acquire()
-                    self.__TOTimer.cancel()
-                    self.__waitingAnswer = False  #DELETEME
+                    #self.__waitingAnswer = False  #DELETEME
+
                     if self.__waitingAnswer:
-                        self.__waitingAnswer = False
+                        answerString += readBytes
+                        answer = self.__currCommand.parseResponse(answerString)
+
+                        # Expected answer
+                        if answer[0]:
+                            self.__TOTimer.cancel()
+                            self.__waitingAnswer = False
+                            answerString = ''
+                            print "Answer: "
+                            print(', '.join(map(str, answer)))
+                            self.receivedAnswer(answer)
                         self.__cmdMutex.release()
+
                     else:
                         self.__cmdMutex.release()
-                        self.receivedData(data)
-                        print >> sys.stdout, data
+                        self.receivedData(readBytes)
+                        print >> sys.stdout, readBytes
                         
         except serial.SerialException:
             self.status = False
 
     def writeRaw(self, data):
-        print >> sys.stdout, "tx", data
-        self.__serial.write(data)
+        if self.status:
+            print >> sys.stdout, "tx", data
+            self.__serial.write(data)
 
 
 def printerCallback(data):
